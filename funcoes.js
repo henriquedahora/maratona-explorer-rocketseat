5db98e41d776e4553f77af8232b059f9a73d005a
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-06-17 13:23:33 -0300

    Adicionando Funções

---
+++ funcoes.js
@@ -0,0 +1,32 @@
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+]
+
+const totalRespostas = respostas.length
+console.log(respostas[totalRespostas])
+
+const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+console.log(numeroAleatorio)
+
+function fazerPergunta () {
+    console.log('cliquei no botao')
+}
